2d5d584534b9954f5203bf49e69c7628108221e6
Eric Karanja <ekaranja@localhost.localdomain>
2017-12-15 10:08:50 +0300

    first commit

---
+++ src/main/java/com/rms/test/test/NewServlet.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rms.test.test;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.Utilities;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ekaranja
+ */
+@WebServlet(name = "NewServlet", urlPatterns = {"/test"})
+public class NewServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+
+            OutputStream file = new FileOutputStream(new File("karanja.pdf"));
+
+            Document document = new Document();
+            Rectangle rec = new Rectangle(Utilities.millimetersToPoints(130), Utilities.millimetersToPoints(100));
+
+            document.setPageSize(rec);
+            PdfWriter.getInstance(document, file);
+            document.open();
+            String name = request.getParameter("tenantName");
+            String amountInWords = UtilityA.convert(Integer.parseInt(request.getParameter("amount")));
+
+            Paragraph p1 = new Paragraph("No:              " + "Ann Mumbi Karanja LTD          " + "Date: "+new Date().toString());
+            Paragraph p2 = new Paragraph("Received From:   " + name);
+            Paragraph p3 = new Paragraph("The sum of shillings: " + amountInWords);
+            Paragraph p4 = new Paragraph("Being payment of: " + "Rent for month of December");
+            Paragraph p5 = new Paragraph("By Cash/Cheque:  " + "for     " + "WITH THANKS");
+            Paragraph p6 = new Paragraph("Shs.  " + request.getParameter("amount")+"/=");
+            document.add(p1);
+            document.add(p2);
+            document.add(p3);
+            document.add(p4);
+            document.add(p5);
+            document.add(p6);
+            document.close();
+            file.close();
+
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet NewServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        } catch (DocumentException ex) {
+            Logger.getLogger(NewServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
